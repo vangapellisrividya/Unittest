d08887721a4e59ec32cb15edefa7448efc830fde
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-12-01 08:45:45 +0530

    [srividya] Refactor: User registration test file

--- test_userlogin.py
+++ test_userlogin.py
@@ -1,12 +1,12 @@
 '''
-@Date:  2021-11-21	
+@Date:  2021-11-28	
 @Author:Srividya
 @Title : test_Userlogin
 
 '''
 
 import unittest
-from user_login import firstname, lastname, email, password, phonenumber
+from user_login import firstname,lastname,email,phonenumber,password
 
 class TestCaseUser(unittest.TestCase):
 
@@ -16,24 +16,27 @@ class TestCaseUser(unittest.TestCase):
         Parameter: self
         Return: None
         """
-        Firstname = firstname('Srividya')
-        self.assertEqual(Firstname, 'Srividya')
+        firstname = 'Srividya'
+        self.assertEqual(firstname,'Srividya')
+
+    
     def test_username2(self):
         """"
         Description: this function will test the lasstname
         Parameter: self
         Return: None
         """
-        Lastname = lastname('Vangapelli')
-        self.assertEqual(Lastname,'Vangapelli')
+        lastname = 'Vangapelli'
+        self.assertEqual(lastname,'Vangapelli')
+
     def test_Email(self):
         """"
         Description: this function will test the E-mail
         Parameter: self
         Return: None
         """
-        Email = email('vangapellisreevidya@gmail.com')
-        self.assertEqual(Email, 'vangapellisreevidya@gmail.com')
+        email = 'vangapellisreevidya@gmail.com'
+        self.assertEqual(email, 'vangapellisreevidya@gmail.com')
 
     def test_PhoneNumber(self):
         """"
@@ -41,8 +44,8 @@ class TestCaseUser(unittest.TestCase):
         Parameter: self
         Return: None
         """
-        PhoneNumber = phonenumber('917036653770')
-        self.assertEqual(PhoneNumber, '917036653770')
+        phonenumber = '917036653770'
+        self.assertEqual(phonenumber,'917036653770')
 
     def test_Password(self):
         """"
@@ -50,8 +53,46 @@ class TestCaseUser(unittest.TestCase):
         Parameter: self
         Return: None
         """
-        Password = password('Sri@123')
-        self.assertEqual(Password, 'Sri@123')
+        password = 'Sri@123'
+        self.assertEqual(password, 'Sri@123')
+
+    def test_values1(self):
+        """"
+        Description: this function will test the firstname
+        Parameter: self
+        Return: None
+        """
+        self.assertRaises(TypeError,firstname(123),123)
+    def test_values2(self):
+        """"
+        Description: this function will test the lasstname
+        Parameter: self
+        Return: None
+        """
+        self.assertRaises(TypeError,lastname(123),123)
+        
+    def test_values3(self):
+        """"
+        Description: this function will test the email
+        Parameter: self
+        Return: None
+        """
+        self.assertRaises(TypeError,email('vidygmail.com'),'vidygmail.com')
+            
+    def test_values4(self):
+        """"
+        Description: this function will test the phonenumber
+        Parameter: self
+        Return: None
+        """
+        self.assertRaises(TypeError,phonenumber('helo'),'helo')
+    def test_values5(self):
+        """"
+        Description: this function will test the password
+        Parameter: self
+        Return: None
+        """
+        self.assertRaises(TypeError ,password('vidya123'),'vidya123')
 
 if __name__=='__main__':
     unittest.main()
